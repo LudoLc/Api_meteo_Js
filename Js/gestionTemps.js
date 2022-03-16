9a3f19cb384503ab472892746e733795f850c9c7
LudoLc <ludlecoy@gmail.com>
2022-03-16 16:58:54 +0100

    First push

---
+++ Js/gestionTemps.js
@@ -0,0 +1,13 @@
+const joursSemaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+let ajd = new Date();
+let option = {weekday: 'long'};
+let jourActuel = ajd.toLocaleDateString('fr-FR', option);
+//console.log(jourActuel,ajd);
+
+jourActuel = jourActuel.charAt(0).toLocaleUpperCase() + jourActuel.slice(1);
+
+let tabJoursEnOrdre = joursSemaine.slice(joursSemaine.indexOf(jourActuel)).concat(joursSemaine.slice(0, joursSemaine.indexOf(jourActuel)));
+//console.log(tabJoursEnOrdre);
+
+export default tabJoursEnOrdre;
